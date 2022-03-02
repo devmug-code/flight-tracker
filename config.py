40198aba1a02402c09818255a24c54906038ce13
Jordan Lee <jordanlee2468@live.com>
2022-03-01 20:32:03 -0500

    inital commit

---
+++ config.py
@@ -0,0 +1,17 @@
+ZONE_HOME = {
+    "tl_y": 34.564879, # Top-Left Latitude (deg)
+    "tl_x": -82.831598, # Top-Left Longitude (deg)
+    "br_y": 34.421817, # Bottom-Right Latitude (deg)
+    "br_x": -82.506025 # Bottom-Right Longitude (deg)
+}
+HOME_LOCATION = [
+    34.5068601,
+    -82.6506264,
+    0.244
+]
+TEMP_LOCATION = "Anderson, SC"
+TEMP_UNITS = "Metric"
+OPEN_WEATHER_API_KEY = "31bc895d4c9030fba03af7814d620389"
+MIN_ALTITUDE = 100
+JOURNEY_CODE_SELECTED = "GSP"
+JOURNEY_BLANK_FILLER = " ? "
